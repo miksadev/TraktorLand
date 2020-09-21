f8e51873a936174701976ce74c55db859dd98e8b
miksadev <miksadev@gmail.com>
2020-09-21 15:25:29 +0200

    Test push

--- pages/webshop/traktori.js
+++ pages/webshop/traktori.js
@@ -5,12 +5,12 @@ export default function Traktori() {
   return (
     <div className={styles.container}>
       
-
         <div className={styles.body}>
             <h1 className={styles.naslov}>WEBSHOP</h1>
             <h3 className={styles.naslovmanji}>DELOVI ZA POLJOPRIVREDNE MASINE</h3>
             <div className={styles.line}></div>
 
+            <Products/>
             <Products/>
         </div>
        
