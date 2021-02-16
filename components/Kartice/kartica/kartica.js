68249b89cc0ec07e88174d4c0a03069a0a8b29fe
miksadev <miksadev@gmail.com>
2021-02-16 11:33:38 +0100

    className fix

--- components/Kartice/kartica/kartica.js
+++ components/Kartice/kartica/kartica.js
@@ -5,7 +5,7 @@ const kartica = (props) => {
     return(
         <div className={styles.kartica}>
             <img style={{top: props.top + "px",width: props.wid + "px" , height: props.hei + "px", left: props.lef + "px"}} className={styles.slika} src={props.src} alt=""/>
-            <div classname={styles.inlineb}>
+            <div className={styles.inlineb}>
                 <h2 className={styles.ime}>{props.children}</h2>
                 <h3 className={styles.natpis}>{props.natpis}</h3>
             </div>
