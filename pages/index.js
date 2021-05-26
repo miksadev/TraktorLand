1b4d354997c9ea384d84ddc41d97c15232863242
Mihajlo <mihajlo@scentronix.com>
2021-05-26 15:57:26 +0200

    meta description

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Logowide from '../components/UI/Logo/logowide';
 import Kategorije from '../components/Kategorije/kategorije';
 import Kartice from '../components/Kartice/kartice';
 import Swiper from '../components/Slider/swiper';
+import Head from 'next/head'
 
 export async function getServerSideProps({req,res}){
   var HOST = process.env.HOST;
@@ -23,7 +24,10 @@ export default function Home({akcije,catdata}) {
 
   return (
     <>
-      
+      <Head>
+        <meta name="description" content="TraktorLand je sajt firme Marjanovic iz pukovca koja se bavi prodajom poljoprivrednih masina i mehanizacije i delova za iste." />
+        <meta property="og:type" content="website" />
+      </Head>
       <div className={styles.body}>
       <img className={styles.traktor} src="traktor.svg" alt=""/>
         <Coninfo/>
